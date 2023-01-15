ace4b5221395e9f1e64c79176385cba65f830a26
sayf-ismail <63899527+sayf-ismail@users.noreply.github.com>
2023-01-15 10:55:05 +0800

    comments endpoint WIP

--- index.js
+++ index.js
@@ -1,32 +1,47 @@
 const express = require('express');
 const app = express();
 const axios = require('axios');
+const rootUrl = 'https://jsonplaceholder.typicode.com';
 
-  app.get('/top-posts', async (req, res) => {
-    try {
-      const rootUrl = 'https://jsonplaceholder.typicode.com';
-      const [commentsResponse, postsResponse] = await axios.all([
-        axios.get(rootUrl+'/comments'),
-        axios.get(rootUrl+'/posts')
-       ]);
-      const comments = commentsResponse.data;
-      const posts = postsResponse.data.map(post => {
-        return {
-          post_id: post.id,
-          post_title: post.title,
-          post_body: post.body,
-          total_number_of_comments: comments.filter(c => c.postId === post.id).length
-        }
+app.get('/top-posts', async (req, res) => {
+  try {
+    const [axiosComments, axiosPosts] = await axios.all([
+      axios.get(rootUrl+'/comments'),
+      axios.get(rootUrl+'/posts')
+     ]);
+    const comments = axiosComments.data;
+    const posts = axiosPosts.data.map(post => {
+      return {
+        post_id: post.id,
+        post_title: post.title,
+        post_body: post.body,
+        total_number_of_comments: comments.filter(c => c.postId === post.id).length
+      }
+    });
+    posts.sort((a, b) => b.total_number_of_comments - a.total_number_of_comments);
+    res.json(posts);
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({ message: 'Unable to fetch the requested data.' });
+  }
+});
+
+app.get('/comments', async(req, res) => {
+  try {
+    const axiosComments = await axios.get(rootUrl + '/comments');
+    const comments = axiosComments.data;
+    const filteredComments = comments.filter(comment => {
+      return Object.keys(req.query).every(key => {
+        return comment[key] === req.query[key];
       });
-      posts.sort((a, b) => b.total_number_of_comments - a.total_number_of_comments);
-      res.json(posts);
-    } catch (error) {
-      console.log(error);
-      res.status(500).json({ message: 'Error fetching data' });
-    }
-  });
+    });
+    res.json(filteredComments);
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({ message: 'Unable to fetch the requested data.' });
+  }
+});
 
 // PORT
 const port = process.env.PORT || 3000;
 app.listen(port, () => console.log(`Listening on port ${port}`));
-
